94f4a190016d351bf9ff1eb3f1c80d30c0c203ac
Yahya <yahya.almishree@gmail.com>
2023-08-11 17:43:40 -0700

    why

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -16,5 +16,8 @@ char *create_xarray(int size);
 char *iterate_zeroes(char *str);
 void get_prod(char *prod, char *mult, int digit, int zeroes);
 void add_nums(char *final_prod, char *next_prod, int next_len);
+void _puts(char *str);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
 
 #endif
