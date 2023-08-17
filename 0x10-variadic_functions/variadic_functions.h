20830f4901f22ff58e86dcfbc99042d3744c35ed
Yahya <yahya.almishree@gmail.com>
2023-08-17 16:42:55 -0700

    GTA

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
+
+#include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
