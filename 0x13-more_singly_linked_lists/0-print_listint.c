f3d73178020b3185d8842adb465f985800231f68
Yahya <yahya.almishree@gmail.com>
2023-08-30 16:09:16 -0700

    hi

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,16 @@
  * @h: head of linklist node
  * Return: the number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t node = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
+		node++;
 		h = h->next;
-		count++;
 	}
-	return (count);
+	return (nod);
 }
