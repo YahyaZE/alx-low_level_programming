520c6efe70718eb4f989606a1a6322751d9cb16c
Yahya <yahya.almishree@gmail.com>
2023-08-30 17:03:13 -0700

    hi

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,9 +1,9 @@
-#include "lists,h"
+#include "lists.h"
 
 /**
  * listint_len - returns the number of elements in a linked lists
  * @h: linked list of type listint_t to traverse
- * Return: the address of the new element, or NULL if it failed
+ * Return: number of nodes
  */
 
 size_t listint_len(const listint_t *h)
