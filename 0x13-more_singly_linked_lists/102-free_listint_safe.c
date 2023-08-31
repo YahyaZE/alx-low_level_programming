56b383d82ffe09fc325bab8f89473a26d439997c
Yahya <yahya.almishree@gmail.com>
2023-08-30 19:14:48 -0700

    h

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,7 +8,7 @@
  * free_listint_safe - frees a linked list
  * @h: pointer to the first node in the linked list
  *
- * Return: number of elements in the freed list
+ * Return: the size of the list that was free’d
  */
 size_t free_listint_safe(listint_t **h)
 {
