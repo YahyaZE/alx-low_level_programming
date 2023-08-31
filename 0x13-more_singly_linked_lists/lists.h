1d0c0347950ebff739653d04a2bf1a4dcddae56c
Yahya <yahya.almishree@gmail.com>
2023-08-30 18:32:07 -0700

    hj

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,5 +32,9 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 int _putchar(char c);
+void free_listp(listp_t **head);
+void free_listp2(listp_t **head);
+
+
 
 #endif /*LISTS_H*/
